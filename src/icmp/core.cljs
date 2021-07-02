dca59cdbd8290ed21a8e52fb66ec7ee59ba0a879
Joel Martin <joel.martin@viasat.com>
2021-07-02 05:51:30 +0000

    Use :choice in ICMP.

--- src/icmp/core.cljs
+++ src/icmp/core.cljs
@@ -7,7 +7,7 @@
 
 (def MSG-TYPE-LIST
   [[ 0  :echo-reply               ]   ;; ping response
-   [ 3  :destination-unreachable  ]
+   [ 3  :dest-unreachable         ]
    [ 4  :source-quench            ]
    [ 5  :redirect                 ]
    [ 8  :echo-request             ] ;; ping
@@ -27,44 +27,29 @@
   (merge (into {} (map (fn [[c n]] [c n]) MSG-TYPE-LIST))
          (into {} (map (fn [[c n]] [n c]) MSG-TYPE-LIST))))
 
-(def ICMP-TYPE-HEADERS
-  ;; name,          type,         length,    extra-context
-  {:echo-request
-   [[:id            :uint16         2        {:default 0}]
-    [:seq-num       :uint16         2        {:default 0}]
-    [:payload       :raw            :*       {}]]
+(def MSG-TYPE-HEADERS
+  ;; msg-type           name,          type,         length,    extra-context
+  {:echo-request      [[:id            :uint16         2        {:default 0}]
+                       [:seq-num       :uint16         2        {:default 0}]
+                       [:payload       :raw            :*       {}]]
+   :echo-reply        [[:id            :uint16         2        {:default 0}]
+                       [:seq-num       :uint16         2        {:default 0}]
+                       [:payload       :raw            :*       {}]]
+   :dest-unreachable  [[:unused        :uint32         4        {:default 0}]
+                       [:orig-packet   :raw            :*       {}]]
+   :redirect          [[:gw-addr       :ipv4           4        {:default ""}]
+                       [:orig-packet   :raw            :*       {}]]})
 
-   :echo-reply
-   [[:id            :uint16         2        {:default 0}]
-    [:seq-num       :uint16         2        {:default 0}]
-    [:payload       :raw            :*       {}]]
-
-   :destination-unreachable
-   [[:unused        :uint32         4        {:default 0}]
-    [:orig-packet   :raw            :*       {}]]
-
-   :redirect
-   [[:gw-addr       :ipv4           4        {:default ""}]
-    [:orig-packet   :raw            :*       {}]]})
+(def MSG-TYPE-MAP
+  (into {} (for [[k v] MSG-TYPE-HEADERS] [k {:choice-type :header :spec v}])))
 
 (def ICMP-HEADER
 ;;  name,          type,         length,    extra-context
   [[:type          :msg-type       1        {:default 0}]
    [:code          :uint8          1        {:default 0}]
    [:checksum      :uint16         2        {:default 0}]
-   [:data          :icmp-data      :*       {:type-headers
-                                             ICMP-TYPE-HEADERS} ]])
-
-(defn read-icmp-data [buf start end ctx]
-  (let [msg-type (get-in ctx [:msg-map :type])
-        type-header (get-in ctx [:type-headers msg-type])]
-    (header/read-header buf start end (assoc ctx :spec type-header))))
-
-(defn write-icmp-data [buf msg-map start ctx]
-  (let [msg-type (get-in ctx [:msg-map :type])
-        type-header (get-in ctx [:type-headers msg-type])
-        ctx (assoc ctx :spec type-header)]
-    (header/write-header buf msg-map start ctx)))
+   [:data          :choice         :*       {:choice-on :type
+                                             :choices MSG-TYPE-MAP} ]])
 
 (set! *warn-on-infer* false)
 
@@ -72,18 +57,18 @@
   (merge
     fields/readers-BE
     addrs/readers
+    header/readers
     {:msg-type  #(let [tnum (.readUInt8 %1 %2)
                        typ (get MSG-TYPE-LOOKUP tnum)]
                   (assert typ (str "Unknown ICMP message type " tnum))
-                  typ)
-     :icmp-data read-icmp-data}))
+                  typ)}))
 
 (def writers
   (merge
     fields/writers-BE
     addrs/writers
-    {:msg-type   #(.writeUInt8 %1 (get MSG-TYPE-LOOKUP %2) %3)
-     :icmp-data  write-icmp-data}))
+    header/writers
+    {:msg-type   #(.writeUInt8 %1 (get MSG-TYPE-LOOKUP %2) %3)}))
 
 (set! *warn-on-infer* true)
 
